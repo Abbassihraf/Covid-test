40efcc31a6f168d8227126d03b839fb6a4b4a63b
Abbassihraf <hraf.abbassi@gmail.com>
2020-05-05 21:54:32 +0000

    Update ar.js

--- Js/ar.js
+++ Js/ar.js
@@ -176,24 +176,170 @@ let severity = 0
 
 function Results() {
 
-    if (answers['Q1'] === 'Oui') {
 
-        severity++
-    }
 
-    if (answers['Q8'] === 'Oui' || answers['Q9'] === 'Oui') {
+//Patient avec fièvre, ou toux + mal de gorge, ou toux + courbatures ou fièvre + diarrhée :
+//Tout patient sans facteur pronostique :Sans facteur de gravité & <50 ans :
 
-        severity++
-    }
+if (((answers['Q1'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q5'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q4'] === 'نعم' || answers['Q1'] === 'نعم') && answers['Q6'] === 'نعم') && (answers['Q14'] === 'لا' && answers['Q15'] === 'لا' && answers['Q16'] === 'لا' && answers['Q17'] === 'لا' && answers['Q18'] === 'لا' && answers['Q19'] === 'لا' && ( answers['Q20'] === 'لا' || answers['Q20'] === 'راجل' ) && answers['Q21'] === 'لا' && answers['Q22'] === 'لا' &&  answers['Q11'] < 50 )) {
 
-    if (answers['Q10'] === 'Fatigué(e)' || answers['Q10'] === 'Très fatigué') {
-        severity++
-    }
+    resultMessage[0].innerText = 'كنصحوك تبقى في الدار وتصوني على طبيب ديالك في حالة ماشكيتي براسك' +
+            ' عاود الاختبار' 
+    resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+    resultMessage[0].style.fontWeight = 'bold'
+    resultMessage[0].style.color = '#369D53'
+}
 
-    if (answers['Q14'] === 'Oui' || answers['Q15'] === 'Oui') {
 
-        severity++
-    }
+//Patient avec fièvre, ou toux + mal de gorge, ou toux + courbatures ou fièvre + diarrhée :
+//Tout patient sans facteur pronostique : Sans facteur de gravité & 50-69 ans, ou avec au moins un facteur de gravité mineur
+
+if  (((answers['Q1'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q5'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q4'] === 'نعم' || answers['Q1'] === 'نعم') && answers['Q6'] === 'نعم') && ((answers['Q14'] === 'لا' && answers['Q15'] === 'لا' && answers['Q16'] === 'لا' && answers['Q17'] === 'لا' && answers['Q18'] === 'لا' && answers['Q19'] === 'لا' && ( answers['Q20'] === 'لا' || answers['Q20'] === 'راجل' ) && answers['Q21'] === 'لا' && answers['Q22'] === 'لا' && answers['Q11'] > 50) || ((answers['Q2'] >= 39) || (answers['Q7'] === 'نعم') || (answers['Q10'] === ' عيان') || (answers['Q10'] === 'مهلوك')))) {
+
+    resultMessage[0].innerText = "تقد تشاور مع طبيب فالتيليفون او يزورك طبيب فالدار" +
+    "إذا كان عندك صعوبة فالتنفس او مقديتيش تاكل مزيان او تشرب لكثر من 24 ساعة ضروري اتصل ب" +
+    "141"
+    resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+     resultMessage[0].style.fontWeight = 'bold'
+     resultMessage[0].style.color = '#369D53'
+}
+
+
+//Patient avec fièvre, ou toux + mal de gorge, ou toux + courbatures ou fièvre + diarrhée :
+//Tout patient avec un facteur pronostique ou plus: Aucun facteur de gravité :
+
+if (((answers['Q1'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q5'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q4'] === 'نعم' || answers['Q1'] === 'نعم') && answers['Q6'] === 'نعم') && ((answers['Q14'] === 'نعم' || answers['Q15'] === 'نعم' || answers['Q16'] === 'نعم' || answers['Q17'] === 'نعم' || answers['Q18'] === 'نعم' || answers['Q19'] === 'نعم' ||answers['Q20'] === 'نعم'  || answers['Q21'] === 'نعم' || answers['Q22'] === 'نعم') && (answers['Q7'] === 'لا' && answers['Q8'] === 'لا' && answers['Q17'] === 'لا' && answers['Q10'] === 'شويا' && answers['Q10'] === 'مزيان'))){
+
+    resultMessage[0].innerText = "تقد تشاور مع طبيب فالتيليفون او يزورك طبيب فالدار" +
+    "إذا كان عندك صعوبة فالتنفس او مقديتيش تاكل مزيان او تشرب لكثر من 24 ساعة ضروري اتصل ب" +
+    "141"
+    resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+     resultMessage[0].style.fontWeight = 'bold'
+     resultMessage[0].style.color = '#369D53'
+}
+
+
+//Patient avec fièvre, ou toux + mal de gorge, ou toux + courbatures ou fièvre + diarrhée :
+//Tout patient avec un facteur pronostique ou plus: Un seul facteur de gravité mineur :  
+
+if (((answers['Q1'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q5'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q4'] === 'نعم' || answers['Q1'] === 'نعم') && answers['Q6'] === 'نعم') && (answers['Q14'] === 'نعم' || answers['Q15'] === 'نعم' || answers['Q16'] === 'نعم' || answers['Q17'] === 'نعم' || answers['Q18'] === 'نعم' || answers['Q19'] === 'نعم' ||answers['Q20'] === 'نعم'  || answers['Q21'] === 'نعم' || answers['Q22'] === 'نعم') && (answers['Q2'] >= 39 || answers['Q7'] === 'نعم' || answers['Q10'] === 'مهلوك' || answers['Q10'] === 'عيان' )){
+
+resultMessage[0].innerText = "تقد تشاور مع طبيب فالتيليفون او يزورك طبيب فالدار" +
+                "إذا كان عندك صعوبة فالتنفس او مقديتيش تاكل مزيان او تشرب لكثر من 24 ساعة ضروري اتصل ب" +
+                "141"
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+ resultMessage[0].style.fontWeight = 'bold'
+ resultMessage[0].style.color = '#369D53'
+}
+
+
+//Patient avec fièvre, ou toux + mal de gorge, ou toux + courbatures ou fièvre + diarrhée :
+//Tout patient avec un facteur pronostique ou plus: deux facteurs de gravité mineurs :
+
+if (((answers['Q1'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q5'] === 'نعم' || answers['Q3'] === 'نعم') && (answers['Q4'] === 'نعم' || answers['Q1'] === 'نعم') && answers['Q6'] === 'نعم') && (answers['Q14'] === 'نعم' || answers['Q15'] === 'نعم' || answers['Q16'] === 'نعم' || answers['Q17'] === 'نعم' || answers['Q18'] === 'نعم' || answers['Q19'] === 'نعم' ||answers['Q20'] === 'نعم'  || answers['Q21'] === 'نعم' || answers['Q22'] === 'نعم') && ((answers['Q2'] >= 39 && answers['Q7'] === 'نعم') || (answers['Q2'] >= 39 && answers['Q10'] === 'عيان') || (answers['Q2'] >= 39 && answers['Q10'] === 'مهلوك') || (answers['Q7'] === 'نعم' && answers['Q10'] === 'عيان') || (answers['Q7'] === 'نعم' && answers['Q10'] === 'مهلوك'))){
+
+    resultMessage[0].innerText = "Appelez le 141"
+    resultMessage[1].innerText = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Pren' +
+            'ez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+    resultMessage[0].style.color = '#FF0000'
+    resultMessage[0].style.fontSize = '48px'
+    resultMessage[0].style.fontWeight = 'bold'
+
+}
+
+
+//Tout patient avec fièvre et toux :
+//Tout patient sans facteur pronostique :
+//Sans facteur de gravité ou au moins 1 facteur de gravité mineur sans facteur de gravité majeur:
+
+if (((answers['Q1'] === 'نعم' && answers['Q3'] === 'نعم') && (answers['Q14'] === 'لا' && answers['Q15'] === 'لا' && answers['Q16'] === 'لا' && answers['Q17'] === 'لا' && answers['Q18'] === 'لا' && answers['Q19'] === 'لا' && ( answers['Q20'] === 'لا' || answers['Q20'] === 'راجل' ) && answers['Q21'] === 'لا' && answers['Q22'] === 'لا')) && (answers['Q7'] === 'لا' && answers['Q8'] === 'لا' && answers['Q17'] === 'لا' && answers['Q10'] === 'شويا' && answers['Q10'] === 'مزيان') || ((answers['Q2'] >= 39 || answers['Q7'] === 'نعم' || answers['Q10'] === 'مهلوك' || answers['Q10'] === 'عيان') && answers['Q2'] <= 35,4 && answers['Q17'] === 'لا' && answers['Q8'] === 'لا')) {
+
+resultMessage[0].innerText = "تقد تشاور مع طبيب فالتيليفون او يزورك طبيب فالدار" +
+                "إذا كان عندك صعوبة فالتنفس او مقديتيش تاكل مزيان او تشرب لكثر من 24 ساعة ضروري اتصل ب" +
+                "141"
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+ resultMessage[0].style.fontWeight = 'bold'
+ resultMessage[0].style.color = '#369D53'
+}
+
+//Tout patient avec fièvre et toux :
+//Tout patient avec un facteur pronostique ou plus :
+//Aucun facteur de gravité:
+
+if ((answers['Q1'] === 'نعم' && answers['Q3'] === 'نعم') && (answers['Q14'] === 'نعم' || answers['Q15'] === 'نعم' || answers['Q16'] === 'نعم' || answers['Q17'] === 'نعم' || answers['Q18'] === 'نعم' || answers['Q19'] === 'نعم' ||answers['Q20'] === 'نعم'  || answers['Q21'] === 'نعم' || answers['Q22'] === 'نعم') && answers['Q7'] === 'لا' && answers['Q8'] === 'لا' && answers['Q17'] === 'لا' && answers['Q10'] === 'شويا ' && answers['Q10'] === 'مزيان') {
+
+resultMessage[0].innerText = "تقد تشاور مع طبيب فالتيليفون او يزورك طبيب فالدار" +
+                "إذا كان عندك صعوبة فالتنفس او مقديتيش تاكل مزيان او تشرب لكثر من 24 ساعة ضروري اتصل ب" +
+                "141"
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+ resultMessage[0].style.fontWeight = 'bold'
+ resultMessage[0].style.color = '#369D53'
+
+}
+
+//Tout patient avec fièvre et toux :
+//Tout patient avec un facteur pronostique ou plus :
+//Un seul facteur de gravité mineur:
+
+if ((answers['Q1'] === 'نعم' && answers['Q3'] === 'نعم') && (answers['Q14'] === 'نعم' || answers['Q15'] === 'نعم' || answers['Q16'] === 'نعم' || answers['Q17'] === 'نعم' || answers['Q18'] === 'نعم' || answers['Q19'] === 'نعم' ||answers['Q20'] === 'نعم'  || answers['Q21'] === 'نعم' || answers['Q22'] === 'نعم') && (answers['Q2'] >= 39 || answers['Q7'] === 'نعم' || answers['Q10'] === 'مهلوك' || answers['Q10'] === 'عيان')) {
+
+resultMessage[0].innerText = "تقد تشاور مع طبيب فالتيليفون او يزورك طبيب فالدار" +
+                "إذا كان عندك صعوبة فالتنفس او مقديتيش تاكل مزيان او تشرب لكثر من 24 ساعة ضروري اتصل ب" +
+                "141"
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+ resultMessage[0].style.fontWeight = 'bold'
+ resultMessage[0].style.color = '#369D53'
+}
+
+//Tout patient avec fièvre et toux :
+//Tout patient avec un facteur pronostique ou plus :
+//Au moins deux facteurs de gravité mineurs:
+
+if (((answers['Q1'] === 'نعم' && answers['Q3'] === 'نعم') && (answers['Q14'] === 'نعم' || answers['Q15'] === 'نعم' || answers['Q16'] === 'نعم' || answers['Q17'] === 'نعم' || answers['Q18'] === 'نعم' || answers['Q19'] === 'نعم' ||answers['Q20'] === 'نعم'  || answers['Q21'] === 'نعم' || answers['Q22'] === 'نعم')) && ((answers['Q2'] >= 39 && answers['Q7'] === 'نعم') || (answers['Q2'] >= 39 && answers['Q10'] === 'عيان') || (answers['Q2'] >= 39 && answers['Q10'] === 'مهلوك') || (answers['Q7'] === 'نعم' && answers['Q10'] === 'عيان') || (answers['Q7'] === 'نعم' && answers['Q10'] === 'مهلوك'))) {
+
+resultMessage[0].innerText = "اتصل ب 141"
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+resultMessage[0].style.color = '#FF0000'
+resultMessage[0].style.fontSize = '48px'
+resultMessage[0].style.fontWeight = 'bold'
+}
+
+//Tout patient avec un seul symptôme parmi fièvre, toux, mal de gorge, courbatures :
+//Pas de facteur de gravité:
+
+if ((answers['Q1'] === 'نعم' ||answers['Q3'] === 'نعم'  || answers['Q5'] === 'نعم' || answers['Q4'] === 'نعم') &&(answers['Q7'] === 'لا' && answers['Q8'] === 'لا' && answers['Q17'] === 'لا' && answers['Q10'] === 'شويا' && answers['Q10'] === 'مزيان')){
+
+resultMessage[0].innerText = 'مافيكش فيروس كورونا على الأرجح، إذا عندك شك اتصل بالطبيب ديالك. ولا صوني ل 141'
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+resultMessage[0].style.fontWeight = 'bold'
+resultMessage[0].style.fontWeight = 'bold'
+resultMessage[0].style.color = '#369D53'
+
+}
+
+//Tout patient avec un seul symptôme parmi fièvre, toux, mal de gorge, courbatures :
+//Au moins un facteur de gravité ou un facteur pronostique:
+
+if ((answers['Q1'] === 'نعم' ||answers['Q3'] === 'نعم'  || answers['Q5'] === 'نعم' || answers['Q4'] === 'نعم') && ((answers['Q14'] === 'نعم' || answers['Q15'] === 'نعم' || answers['Q16'] === 'نعم' || answers['Q17'] === 'نعم' || answers['Q18'] === 'نعم' || answers['Q19'] === 'نعم' ||answers['Q20'] === 'نعم'  || answers['Q21'] === 'نعم' || answers['Q22'] === 'نعم') || (answers['Q7'] === 'نعم' && answers['Q8'] === 'نعم' && answers['Q17'] === 'نعم' && answers['Q10'] === 'مهلوك' && answers['Q10'] === 'عيان' ) )) {
+
+resultMessage[0].innerText = 'مافيكش فيروس كورونا على الأرجح، إذا عندك شك اتصل بالطبيب ديالك. ولا صوني ل 141'
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+resultMessage[0].style.fontWeight = 'bold'
+resultMessage[0].style.fontWeight = 'bold'
+resultMessage[0].style.color = '#369D53'
+}
+
+
+// Tout patient avec aucun symptôme :
+
+if ((answers['Q1'] === 'لا') && (answers['Q3'] === 'لا') && (answers['Q4'] === 'لا') && (answers['Q5'] === 'لا') && (answers['Q6'] === 'لا')){
+
+resultMessage[0].innerText = 'مافيكش فيروس كورونا على الأرجح، إذا عندك شك اتصل بالطبيب ديالك. تقد تعاود الإختبار لكانوا عندك اعراض اخرى. لبغيتي تعرف اكثر على الڤيروس ممكن تشوف صفحة النصائح'
+resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
+resultMessage[0].style.fontWeight = 'bold'
+resultMessage[0].style.fontWeight = 'bold'
+resultMessage[0].style.color = '#369D53'
+}
 
     showResult(severity)
 
@@ -220,55 +366,9 @@ function showResult(severity) {
 
     result.innerText = 'النتائج'
 
-    if (severity === 0) {
 
-        resultMessage[0].innerText = 'مافيكش فيروس كورونا على الأرجح، إذا عندك شك اتصل بالطبيب ديالك. تقد تعاود الإختبار لكانوا عندك اعراض اخرى. لبغيتي تعرف اكثر على الڤيروس ممكن تشوف صفحة النصائح'
-        resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
-        resultMessage[0].style.fontWeight = 'bold'
-        resultMessage[0].style.color = '#369D53'
-    } else if (severity === 1) {
-
-        resultMessage[0].innerText = 'Nous vous conseillons de rester à votre domicile et de contacter votre médecin' +
-                ' en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouv' +
-                'eau l’application pour réévaluer vos symptômes'
-        resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
-        resultMessage[0].style.fontWeight = 'bold'
-        resultMessage[0].style.color = '#369D53'
-
-    } else if (severity === 2) {
-        resultMessage[0].innerText = "تقد تشاور مع طبيب فالتيليفون او يزورك طبيب فالدار" +
-                "إذا كان عندك صعوبة فالتنفس او مقديتيش تاكل مزيان او تشرب لكثر من 24 ساعة ضروري اتصل ب" +
-                "141"
-        resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
-        resultMessage[0].style.fontWeight = 'bold'
-        resultMessage[0].style.color = '#369D53'
-    } else {
-        resultMessage[0].innerText = "Appelez le 141"
-        resultMessage[1].innerText = 'خليك فدارك حتى تحيد الاعراض، عبر درجة الحراة 2 مرات فالنهار. ومتنساوش النظافة و غسل إيديك بزاف ديال المرات فالنهار'
-        resultMessage[0].style.color = '#FF0000'
-        resultMessage[0].style.fontSize = '48px'
-        resultMessage[0].style.fontWeight = 'bold'
-
-    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
